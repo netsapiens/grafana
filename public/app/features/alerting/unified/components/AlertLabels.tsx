64880c740ee809490ed7855e46699383c03c8615
Ryan Tannenberg <54419106+ryantannenberg@users.noreply.github.com>
2022-12-14 16:02:56 -0800

    hide label keys by default

--- public/app/features/alerting/unified/components/AlertLabels.tsx
+++ public/app/features/alerting/unified/components/AlertLabels.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import { TagList } from '@grafana/ui';
 
-type Props = { labels: Record<string, string>; className?: string };
+type Props = { labels: Record<string, string>; className?: string; showLabelKeys?: boolean };
 
-export const AlertLabels = ({ labels, className }: Props) => {
+export const AlertLabels = ({ labels, className, showLabelKeys }: Props) => {
   const pairs = Object.entries(labels).filter(([key]) => !(key.startsWith('__') && key.endsWith('__')));
   return (
     <div className={className}>
-      <TagList tags={Object.values(pairs).map(([label, value]) => `${label}=${value}`)} />
+      <TagList tags={Object.values(pairs).map(([label, value]) => (showLabelKeys ? `${label}=${value}` : value))} />
     </div>
   );
 };
